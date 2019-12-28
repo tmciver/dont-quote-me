4c38db44c1e601cb36fbc961f741c130563da899
Tim McIver <tim@timmciver.com>
2019-12-27 20:35:43 -0500

    Moved HTML-related functions to their own View.Html module.

--- app/Main.hs
+++ app/Main.hs
@@ -6,38 +6,11 @@ import Web.Scotty
 import Domain.Quote
 import Domain.QuoteRepository
 import qualified View.Quote as V
+import qualified View.Html as HTML
 import Data.Text as T
-import Control.Monad (forM_)
 import Control.Monad.IO.Class
-import Data.String (fromString)
-import qualified Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes
-import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Network.URL (importURL)
-
-quoteToListItemText :: V.ViewQuote -> T.Text
-quoteToListItemText (V.ViewQuote quote quotee) = T.concat ["\"", quote, "\" - ", T.pack $ show quotee]
-
-quoteHtml :: V.ViewQuote -> H.Html
-quoteHtml = H.li . fromString . T.unpack . quoteToListItemText
-
-quotesHtml :: [V.ViewQuote] -> H.Html
-quotesHtml quotes = do
-  H.p "Quotes:"
-  --H.ul $ forM_ quotes (H.li . fromString . T.unpack . desc)
-  H.ul $ forM_ quotes quoteHtml
-
-quoteForm :: H.Html
-quoteForm = H.form H.! method "post" H.! enctype "multipart/form-data" H.! action "/quotes" $ do
-  H.span $ H.toHtml ("Enter a quote:" :: Text)
-  H.br
-  H.input H.! type_ "textarea" H.! name "quote"
-  H.br
-  H.span $ H.toHtml ("Who said it? (Enter \"Anonymous\" or a URL that returns .ttl):" :: Text)
-  H.br
-  H.input H.! type_ "textarea" H.! name "said_by"
-  H.br
-  H.input H.! type_ "submit" H.! value "Submit"
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 createQuote :: QuoteRepository
            -> Text
@@ -61,23 +34,11 @@ handleQuotePost repo = do
     Nothing -> raise "Invalid quotee"
     Just quotee -> createQuote repo quoteText' quotee
 
-homeLink :: H.Html
-homeLink = ((H.a . H.toHtml) ("Home" :: String)) H.! href "/"
-
-template :: String -> H.Html -> H.Html
-template title' body' = H.docTypeHtml $ do
-  H.head $ do
-    H.title $ fromString $ "Don't Quote Me! - " ++ title'
-  H.body $ homeLink >> H.br >> body'
-
 homeView :: QuoteRepository -> ActionM ()
 homeView repo = do
   quotes <- liftIO $ getAll repo
   viewQuotes <- liftIO $ traverse V.toView quotes
-  html $ renderHtml $ homeHtml viewQuotes
-
-homeHtml :: [V.ViewQuote] -> H.Html
-homeHtml quotes = template "Home" quoteForm >> (quotesHtml quotes)
+  html $ renderHtml $ HTML.homeHtml viewQuotes
 
 main :: IO ()
 main = do
---
+++ app/View/Html.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module View.Html where
+
+import qualified Text.Blaze.Html5 as H
+import qualified View.Quote as V
+import Data.Text as T
+import Control.Monad (forM_)
+import Text.Blaze.Html5.Attributes
+import Data.String (fromString)
+
+quoteToListItemText :: V.ViewQuote -> T.Text
+quoteToListItemText (V.ViewQuote quote quotee) = T.concat ["\"", quote, "\" - ", T.pack $ show quotee]
+
+quoteHtml :: V.ViewQuote -> H.Html
+quoteHtml = H.li . fromString . T.unpack . quoteToListItemText
+
+quotesHtml :: [V.ViewQuote] -> H.Html
+quotesHtml quotes = do
+  H.p "Quotes:"
+  --H.ul $ forM_ quotes (H.li . fromString . T.unpack . desc)
+  H.ul $ forM_ quotes quoteHtml
+
+quoteForm :: H.Html
+quoteForm = H.form H.! method "post" H.! enctype "multipart/form-data" H.! action "/quotes" $ do
+  H.span $ H.toHtml ("Enter a quote:" :: Text)
+  H.br
+  H.input H.! type_ "textarea" H.! name "quote"
+  H.br
+  H.span $ H.toHtml ("Who said it? (Enter \"Anonymous\" or a URL that returns .ttl):" :: Text)
+  H.br
+  H.input H.! type_ "textarea" H.! name "said_by"
+  H.br
+  H.input H.! type_ "submit" H.! value "Submit"
+
+homeLink :: H.Html
+homeLink = ((H.a . H.toHtml) ("Home" :: String)) H.! href "/"
+
+template :: String -> H.Html -> H.Html
+template title' body' = H.docTypeHtml $ do
+  H.head $ do
+    H.title $ fromString $ "Don't Quote Me! - " ++ title'
+  H.body $ homeLink >> H.br >> body'
+
+homeHtml :: [V.ViewQuote] -> H.Html
+homeHtml quotes = template "Home" quoteForm >> (quotesHtml quotes)
