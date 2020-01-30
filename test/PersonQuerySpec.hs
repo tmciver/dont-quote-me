400b3a8bc2b5f6033c268d2793fa05be635e9c3f
Tim McIver <tim@timmciver.com>
2020-01-29 22:55:07 -0500

    Added tests for `InMemoryPersonQuery` and implemented properly.

--- test/PersonQuerySpec.hs
+++ test/PersonQuerySpec.hs
@@ -7,11 +7,19 @@ import Control.Monad.IO.Class (liftIO)
 
 spec :: Spec
 spec = do
+  describe "InMemoryPersonQuery" $ do
+    let pq = PQ.create
+    it "should return no results for a query that does not match any `Person`" $ do
+      r <- liftIO $ pq "ab"
+      r `shouldBe` []
+    it "should return multiple matching results" $ do
+      r <- liftIO $ pq "mal"
+      length r `shouldBe` 2
   describe "QueryLengthConstraint" $ do
     let minChars = 3
         pq = QLC.create PQ.create minChars
     it "should work normally when the query is at least the minimum length" $ do
-      r <- liftIO $ pq "abc"
-      length r `shouldBe` 1
+      r <- liftIO $ pq "mal"
+      length r `shouldBe` 2
     it "should throw an exception when the query is too short" $ do
-      pq "ab" `shouldThrow` (== QLC.TooFewQueryChars minChars)
+      pq "ma" `shouldThrow` (== QLC.TooFewQueryChars minChars)
