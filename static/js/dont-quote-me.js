034bdf6f59e031071221c57b078e1206b237ca96
Tim McIver <tim@timmciver.com>
2020-02-01 14:00:19 -0500

    Updated JS to query backend.

--- static/js/dont-quote-me.js
+++ static/js/dont-quote-me.js
@@ -1,30 +1,15 @@
 window.onload = function () {
   $('#quotee-text').autocomplete({
-    source: [
-      {
-        label: 'Malcolm Reynolds',
-        value: ' http://dbpedia.org/resource/Malcolm_Reynolds'
-      },
-      {
-        label: 'Abraham Lincoln',
-        value: 'http://dbpedia.org/resource/Abraham_Lincoln'
-      },
-      {
-        label: 'Walt Disney',
-        value: ' http://dbpedia.org/resource/Walt_Disney'
-      },
-      {
-        label: 'Ayn Rand',
-        value: 'http://dbpedia.org/resource/Ayn_Rand'
-      },
-      {
-        label: 'Milton Friedman',
-        value: 'http://dbpedia.org/resource/Milton_Friedman'
-      },
-      {
-        label: 'John Milton',
-        value: 'http://dbpedia.org/resource/John_Milton'
-      },
-    ]
+    source: function(req, callback) {
+      $.get('/query', {'q': req.term})
+        .done(function(data) {
+          const acVals = data.map(d => ({
+            label: d.name,
+            value: d.uri
+          }));
+          callback(acVals);
+        });
+    },
+    minLength: 3
   });
 };
