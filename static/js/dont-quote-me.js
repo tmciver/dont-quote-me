2925b77302a09510d7a9a361f889823a7cc4fc2b
Tim McIver <tim@timmciver.com>
2020-02-01 11:32:16 -0500

    Use array of objects rather than array of strings for autocomplete source.

--- static/js/dont-quote-me.js
+++ static/js/dont-quote-me.js
@@ -1,7 +1,30 @@
 window.onload = function () {
   $('#quotee-text').autocomplete({
-    source: ['Malcolm Reynolds',
-             'Malcolm Bishop',
-             'Abraham Lincoln']
+    source: [
+      {
+        label: 'Malcolm Reynolds',
+        value: ' http://dbpedia.org/resource/Malcolm_Reynolds'
+      },
+      {
+        label: 'Abraham Lincoln',
+        value: 'http://dbpedia.org/resource/Abraham_Lincoln'
+      },
+      {
+        label: 'Walt Disney',
+        value: ' http://dbpedia.org/resource/Walt_Disney'
+      },
+      {
+        label: 'Ayn Rand',
+        value: 'http://dbpedia.org/resource/Ayn_Rand'
+      },
+      {
+        label: 'Milton Friedman',
+        value: 'http://dbpedia.org/resource/Milton_Friedman'
+      },
+      {
+        label: 'John Milton',
+        value: 'http://dbpedia.org/resource/John_Milton'
+      },
+    ]
   });
 };
