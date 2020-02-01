296122d7d8f520ef13dd4c065da7f37fda79ec17
Tim McIver <tim@timmciver.com>
2020-02-01 15:58:04 -0500

    Change `InMemoryPersonQuery` to return `Person`s when name query string is found anywhere in the name, not just at the beginning.

--- src/DontQuoteMe/Domain/PersonQuery/InMemoryPersonQuery.hs
+++ src/DontQuoteMe/Domain/PersonQuery/InMemoryPersonQuery.hs
@@ -9,7 +9,7 @@ create :: PersonQuery
 create query = pure $ filter nameMatches personDb
   where qText = T.toLower $ T.pack query
         nameMatches :: Person -> Bool
-        nameMatches (Person {name=name}) = T.isPrefixOf qText (T.toLower name)
+        nameMatches (Person {name=name}) = qText `T.isInfixOf` (T.toLower name)
         personDb = [ Person "http://dbpedia.org/resource/Malcolm_Reynolds" "Malcolm Reynolds"
                    , Person "http://dbpedia.org/resource/Malcolm_O'Kelly" "Malcolm O'Kelly"
                    , Person "http://dbpedia.org/resource/Jeffrey_Lebowski" "The Dude"]
